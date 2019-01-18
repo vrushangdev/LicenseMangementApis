5e768a9d2501f3c955a2636fd55a381b777217a9
Vrushang Desai <vrushangapple@icloud.com>
2019-01-19 01:17:03 +0530

    changed to mysqldb

--- licensemanagement/settings.py
+++ licensemanagement/settings.py
@@ -79,12 +79,15 @@ WSGI_APPLICATION = 'licensemanagement.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'',
+        'PORT':'',
+        'USER':'Vrushangnew',
+        'PASSWORD':'123456',
+        'NAME': 'django_db'
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
