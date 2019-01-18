fd64101da5c498fd3c6f2fe7b4293e85eb4bde42
Vrushang Desai <vrushangapple@icloud.com>
2019-01-19 01:30:39 +0530

    Moved To /api

--- licensemanagement/urls.py
+++ licensemanagement/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('userlogin.urls')),
-    path('',include('admincontracts.urls')),
+    path('api/',include('admincontracts.urls')),
 
 ]
